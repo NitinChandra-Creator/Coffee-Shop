e2c47f023e98221b51d463f99dda95cc7da43de1
NitinChandra-Creator <nravuroopula@rivier.edu>
2024-08-11 11:14:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        alert(`Thank you, ${name}! Your message has been sent.`);
+
+        // Reset the form
+        form.reset();
+    });
+});
